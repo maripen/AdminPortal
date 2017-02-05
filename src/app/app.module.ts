e1a5e704a9442b2f2ffac64f5ef49ee00053e433
Tamas Nemeth <maripen@gmail.com>
2017-02-05 09:48:45 +0100

    appointment service frontend added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {UserAccountComponent} from "./user-account/user-account.component";
 import {UserService} from "./user.service";
 import { PrimaryTransactionComponent } from './primary-transaction/primary-transaction.component';
 import { SavingsTransactionComponent } from './savings-transaction/savings-transaction.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import {AppointmentService} from "./appointment.service";
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { SavingsTransactionComponent } from './savings-transaction/savings-trans
     LoginComponent,
     UserAccountComponent,
     PrimaryTransactionComponent,
-    SavingsTransactionComponent
+    SavingsTransactionComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +32,8 @@ import { SavingsTransactionComponent } from './savings-transaction/savings-trans
   ],
   providers: [
     LoginService,
-    UserService
+    UserService,
+    AppointmentService
   ],
   bootstrap: [AppComponent]
 })
